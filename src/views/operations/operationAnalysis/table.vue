9c32b14534d49286ab3b5e163812444f0691f4e0
BI-CDP-Yu_Lan <yu_lan@mail.chungyo.net>
2025-06-02 14:27:36 +0800

    [add] 營運分析圖-總報表

---
+++ src/views/operations/operationAnalysis/table.vue
@@ -0,0 +1,70 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import overallReportTable from '@/views/operations/operationAnalysis/table/overallReportTable.vue'
+// import gameTable from '@/views/operations/overallReport/table/gameTable.vue'
+// import playTable from '@/views/operations/overallReport/table/playTable.vue'
+// import dateTable from '@/views/operations/overallReport/table/dateTable.vue'
+import { useOperationsOverallReportStore } from '@/stores'
+import { formatToApiDate } from '@/utils/commonUtils'
+import dayjs from '@/utils/appDayjs'
+
+const { t } = useI18n()
+
+const operationsOverallReportStore = useOperationsOverallReportStore()
+const { searchParams } = operationsOverallReportStore
+
+const buttonGroup = computed(() => [
+  { name: t('sidebar.overall_report'), value: 'overallReportTable' },
+  { name: t('common.hall_master'), value: 'hallTable' },
+  { name: t('common.game'), value: 'gameTable' }
+])
+
+const currentTabs = ref<string>('overallReportTable')
+
+// 整理所有 component
+const componentMap = {
+  overallReportTable,
+  // gameTable,
+  // playTable,
+  // dateTable
+}
+
+// 當前使用的 component
+const currentTabComponent = computed(() => {
+  return componentMap[currentTabs.value] || null
+})
+
+const keepAliveKey = ref(0)
+// watch(
+//   () => operationsOverallReportStore.isFiltered,
+//   () => {
+//     // 產生帶入 api 的基本參數
+//     operationsOverallReportStore.tableBaseParams = getParamInfo()
+//     keepAliveKey.value = operationsOverallReportStore.isFiltered
+//   }
+// )
+</script>
+<template>
+  <section class="cdp-section !p-4">
+    <cdp-button-group class="!mb-4" :buttonGroup="buttonGroup" v-model="currentTabs" />
+    <keep-alive :key="keepAliveKey">
+      <component :is="currentTabComponent"></component>
+    </keep-alive>
+  </section>
+</template>
+<style lang="scss" scoped>
+// // 無資料時 table 的高度
+// // 第一層
+// :deep(.ant-table-placeholder) {
+//   height: 400px;
+// }
+// // 內層
+// :deep(.ant-table-expanded-row) {
+//   .ant-table-placeholder {
+//     height: 100px !important;
+//   }
+//   .ant-pagination {
+//     margin-top: 15px !important;
+//   }
+// }
+</style>
